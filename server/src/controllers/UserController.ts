adf4e855ed6a129b99a6b2055c7af9235522cf16
Mateus Marques <mateusmarques200@gmail.com>
2023-06-16 00:38:43 -0300

    FEAT: returning access and refresh tokens for User

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -2,15 +2,17 @@ import { Request, Response } from "express";
 import { JWT_SECRET } from "../util/secrets";
 import { JWTService } from "../services/JWTService";
 import IUser from "../models/types/IUser";
-import { accessTokenTimeToExpire } from "../config/auth";
+import { accessTokenTimeToExpire, refreshTokenTimeToExpire } from "../config/auth";
 import { BaseController } from "./baseController";
 export class UserController extends BaseController<IUser>{
 
   public post = async (req: Request, res: Response): Promise<void> => {
     const newUser = await this.repository.create(req.body)
 
-    const tokenService = new JWTService(JWT_SECRET!, accessTokenTimeToExpire)
-    let token = tokenService.generate({user: newUser._id})
-    res.status(200).json({token: token})
+    const accessTokenService = new JWTService(JWT_SECRET!, accessTokenTimeToExpire)
+    const refreshTokenService = new JWTService(JWT_SECRET!, refreshTokenTimeToExpire)
+    const accessToken = accessTokenService.generate({user: newUser._id})
+    const refreshToken = refreshTokenService.generate({user: newUser._id})
+    res.status(200).json({accessToken, refreshToken})
   }
 }
