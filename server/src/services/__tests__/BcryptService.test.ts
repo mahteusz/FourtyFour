ef9a58804766ac35fb20e34165120b8d93bf2de0
Mateus Marques <mateusmarques200@gmail.com>
2023-07-04 00:59:23 -0300

    FEAT: finishing BcryptService tests

--- server/src/services/__tests__/BcryptService.test.ts
+++ server/src/services/__tests__/BcryptService.test.ts
@@ -1,16 +1,35 @@
-import BcryptService from "../BcryptService"
-import saltRounds from "../../config/encrypt"
+import BcryptService from "@services/BcryptService"
+import saltRounds from "@config/encrypt"
 import bcrypt from 'bcrypt'
 
 const bcryptService = new BcryptService(saltRounds)
+const generateRandomString = () => {
+  return Math.random().toString(36)
+}
 
 describe("Encryptation", () => {
   it('should encrypt the same as using Bcrypt directly', async () => {
-    const string = "just-a-test"
-    const serviceEncrypted = await bcryptService.encrypt(string)
-    const encryptedDirectly = await bcrypt.hash(string, saltRounds)
-    const serviceEncryptedResult = await bcrypt.compare(string, serviceEncrypted)
-    const encryptedDirectlyResult = await bcrypt.compare(string, encryptedDirectly)
+    const randomString = generateRandomString()
+    const serviceEncrypted = await bcryptService.encrypt(randomString)
+    const encryptedDirectly = await bcrypt.hash(randomString, saltRounds)
+    const serviceEncryptedResult = await bcrypt.compare(randomString, serviceEncrypted)
+    const encryptedDirectlyResult = await bcrypt.compare(randomString, encryptedDirectly)
     expect(serviceEncryptedResult).toBe(encryptedDirectlyResult)
+  })
+})
+
+describe('Comparing', () => {
+  it('should return true when the encrypted text and text are equal', async() => {
+    const randomString = generateRandomString()
+    const encrypted = await bcryptService.encrypt(randomString)
+    const comparing = await bcryptService.compare(randomString, encrypted)
+    expect(comparing).toBe(true)
+  })
+
+  it('should return false when the encrypted text and text are different', async () => {
+    const randomString = generateRandomString()
+    const encrypted = await bcryptService.encrypt(randomString)
+    const comparing = await bcryptService.compare("a-different-random-string", encrypted)
+    expect(comparing).toBe(false)
   })
 })
