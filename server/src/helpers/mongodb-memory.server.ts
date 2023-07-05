17057e6b57886a4eb9fc573d84a475257bb04593
Mateus Marques <mateusmarques200@gmail.com>
2023-07-05 02:23:13 -0300

    FEAT: creating mongo-memory-server helper functions

---
+++ server/src/helpers/mongodb-memory.server.ts
@@ -0,0 +1,15 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongoDb: MongoMemoryServer
+
+export const connect = async () => {
+  mongoDb = await MongoMemoryServer.create()
+  const uri = mongoDb.getUri()
+  await mongoose.connect(uri)
+}
+
+export const disconnect = async () => {
+  await mongoose.disconnect()
+  await mongoDb.stop()
+}
