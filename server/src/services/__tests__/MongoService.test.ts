c97776162c35d07a92080b77d82cbf7fa78db3fc
Mateus Marques <mateusmarques200@gmail.com>
2023-07-06 13:45:10 -0300

    TEST: writing MongoService "read" tests

--- server/src/services/__tests__/MongoService.test.ts
+++ server/src/services/__tests__/MongoService.test.ts
@@ -8,8 +8,6 @@ const createTestDoc = async () => {
   return testDoc
 }
 
-
-
 beforeAll(async () => await connect())
 afterAll(async () => await disconnect())
 const mongoService = new MongoService<ITest>(TestModel)
@@ -18,11 +16,11 @@ describe('Creating documents', () => {
   it('should create a document if everything is ok', async () => {
     const newTest = { stringField: 'simple-test', numberField: 0 } as ITest
 
-    const test = await mongoService.create(newTest)
-    expect(test._id).toBeInstanceOf(ObjectId)
-    expect(test.stringField).toBe(newTest.stringField)
-    expect(test.numberField).toBe(newTest.numberField)
-    expect(test.optionalField).toBe(newTest.optionalField)
+    const doc = await mongoService.create(newTest)
+    expect(doc._id).toBeInstanceOf(ObjectId)
+    expect(doc.stringField).toBe(newTest.stringField)
+    expect(doc.numberField).toBe(newTest.numberField)
+    expect(doc.optionalField).toBe(newTest.optionalField)
   })
 
   it('should throw an error if a required field is missing', async () => {
@@ -74,5 +72,32 @@ describe('Deleting documents', () => {
   it('should return false and not delete the document if id is not found/invalid', async () => {
     const deleted = await mongoService.delete("not-found-id")
     expect(deleted).toBe(false)
+  })
+})
+
+describe('Reading documents', () => {
+  let id: string
+  beforeEach(async () => {
+    const testDoc = await createTestDoc()
+    id = testDoc._id
+  })
+
+  afterEach(async () => {
+    await clearCollections()
+  })
+
+  it('should return an array with all documents if everything is ok', async () => {
+    const docs = await mongoService.find()
+    expect(docs).toHaveLength(1)
+  })
+
+  it('should return a doc if everything is ok', async () => {
+    const doc = await mongoService.findOne(id)
+    expect(doc).not.toBeNull()
+  })
+
+  it('should return null if id is not found/invalid', async () => {
+    const doc = await mongoService.findOne('not-found-id')
+    expect(doc).toBeNull()
   })
 })
